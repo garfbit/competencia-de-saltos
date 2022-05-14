e903052aae76cfee419370e3b8028beef6067df8
garfbit <104676660+garfbit@users.noreply.github.com>
2022-05-14 14:25:18 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.FreeFall, function () {
+    if (!(game.isGameOver())) {
+        Saltos += 1
+        game.setScore(Saltos)
+        basic.showNumber(Saltos)
+    }
 })
+let Saltos = 0
+game.setScore(0)
+Saltos = 0
+game.startCountdown(8000)
